ffb08178b604a3c94d7822dcc8c7ea0c4d143a69
Jibao Mansaray <jibaomansaray@gmail.com>
2022-07-31 09:21:29 +0000

    Setup dotenv package
    - Add nodemon
    - Commented out insertion of dummy data

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -1,17 +1,40 @@
 import "reflect-metadata"
+import { config } from 'dotenv'
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+
+config()
+
+export const env = (name: string, defaultValue: unknown = null) => {
+    const value = process.env[name]
+    if (value) {
+        const cast = {
+            'true': true,
+            'false': false,
+        }
+
+        const toLower = value.toString().toLocaleLowerCase();
+        if (cast[toLower] !== undefined) {
+            return cast[toLower];
+        }
+    }
+
+    return value || defaultValue
+}
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
-    host: "localhost",
-    port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
-    synchronize: true,
-    logging: false,
-    entities: [User],
-    migrations: [],
-    subscribers: [],
+    type: env('DATABASE_DRIVER', 'mysql'),
+    host: env('DATABASE_HOST', 'db'),
+    port: env('DATABASE_PORT', 3306),
+    username: env('DATABASE_USERNAME', 'root'),
+    password: env('DATABASE_PASSWORD', 'dbpassword'),
+    database: env('DATABASE_DATABASE', 'worldcup'),
+    synchronize: env('DATABASE_SYNCHRONIZE', false),
+    logging: env('DATABASE_LOGGING', false),
+    entities: ["src/entity/**/*.ts"],
+    migrations: [
+        "src/migration/**/*.ts"
+    ],
+    subscribers: [
+        "src/subscriber/**/*.ts"
+    ],
 })
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,53 @@
-import { AppDataSource } from "./data-source"
-import { User } from "./entity/User"
+import * as express from "express"
+import * as bodyParser from "body-parser"
+import { Request, Response } from "express"
+import { AppDataSource, env } from "./data-source"
+import { Routes } from "./routes"
+// import { User } from "./entity/User"
 
 AppDataSource.initialize().then(async () => {
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+    // create express app
+    const app = express()
+    app.use(bodyParser.json())
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
+    // register express routes from defined application routes
+    Routes.forEach(route => {
+        (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
+            const result = (new (route.controller as any))[route.action](req, res, next)
+            if (result instanceof Promise) {
+                result.then(result => result !== null && result !== undefined ? res.send(result) : undefined)
 
-    console.log("Here you can setup and run express / fastify / any other framework.")
+            } else if (result !== null && result !== undefined) {
+                res.json(result)
+            }
+        })
+    })
+
+    // setup express app here
+    // ...
+
+    // start express server
+    const PORT = env('SERVER_PORT', 3000);
+    app.listen(PORT)
+
+    // insert new users for test
+    /* await AppDataSource.manager.save(
+        AppDataSource.manager.create(User, {
+            firstName: "Timber",
+            lastName: "Saw",
+            age: 27
+        })
+    )
+
+    await AppDataSource.manager.save(
+        AppDataSource.manager.create(User, {
+            firstName: "Phantom",
+            lastName: "Assassin",
+            age: 24
+        })
+    ) */
+
+    console.log(`Express server has started on port ${PORT}`)
 
 }).catch(error => console.log(error))
