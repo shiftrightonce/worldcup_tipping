021a2c2966ad37e896569d32a8b891e79c916fde
Jibao Mansaray <jibaomansaray@gmail.com>
2022-08-04 06:44:06 +0000

    Add a before insert method
    This method hashes the user password and generate their token.

--- backend/src/entity/User.ts
+++ backend/src/entity/User.ts
@@ -12,16 +12,31 @@ export class User {
     @PrimaryGeneratedColumn()
     id: number
 
+    @Column({
+        type: 'enum',
+        enum: UserRole,
+        default: UserRole.USER
+    })
+    role: UserRole;
+
     @Column({ length: 255 })
     username: string
 
-    @Column({ length: 255, default: ''})
+    @Column({ length: 255, default: '' })
     email: string;
 
-    @Column({length: 128 })
+    @Column({ length: 128 })
     password: string;
 
-    @Column({length: 64 })
+    @Column({ length: 64 })
     token: string
+
+    @BeforeInsert()
+    async handeBeoreInsert () {
+        if (this.password) {
+            this.password = await hashPassword(this.password);
+        }
+        this.token = generateToken();
+    }
 
 }
