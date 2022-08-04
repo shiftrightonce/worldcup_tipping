c220ad9d6ccd3076ed81a6b33685241c0d0d5a7e
Jibao Mansaray <jibaomansaray@gmail.com>
2022-08-04 06:50:13 +0000

    Add function that generate points for team in group stage

---
+++ backend/src/bot/group_round_bot.ts
@@ -0,0 +1,21 @@
+import { Country, Match, YearData, GroupWinnerAndRunnerUp } from '../games/parser/types';
+
+export const generateRandomGroupPoints = (yearData: YearData) => {
+  const groups: { [key: string]: Country[] } = JSON.parse(JSON.stringify(yearData.groups));
+
+  for (const groupIndex in groups) {
+    let maxPoints = 10;
+    const generatedPoints = {};
+    groups[groupIndex].forEach((team) => {
+      let points = Math.floor(Math.random() * maxPoints);
+      while (generatedPoints[points] !== undefined) {
+        points = Math.floor(Math.random() * maxPoints);
+      }
+      generatedPoints[points] = true;
+
+      (team as Country & { groupPoints: number }).groupPoints = points
+    })
+  }
+
+  return groups;
+}
