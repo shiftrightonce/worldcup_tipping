ff6796f7d37f9013e79d271f9f5f2729a0957aa1
Jibao Mansaray <jibaomansaray@gmail.com>
2022-08-16 04:12:21 +0000

    Setup redis and socketio

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,12 +5,32 @@ import * as cookieParser from 'cookie-parser'
 import * as  cors from 'cors'
 import setupApiRoutes from './routes/api_routes'
 import setupPublicRoutes from './routes/public_routes'
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+import { createAdapter } from '@socket.io/redis-adapter'
+import { createClient } from 'redis'
 
 
 AppDataSource.initialize().then(async () => {
 
     // create express app
     const app = express();
+    const httpServer = createServer(app)
+    const io = new Server(httpServer, {
+        cors: {
+            origin: env('SOCKETIO_CORS', '').split(',')
+        }
+    })
+
+    // redis adapter
+    const socketIOPubClient = createClient({ url: env('REDIS_URL', 'redis://redisdbbad') })
+    const socketIOSubClient = socketIOPubClient.duplicate()
+    io.adapter(createAdapter(socketIOPubClient, socketIOSubClient))
+
+    io.on("connection", (socket) => {
+        console.log("connection: " + socket.id)
+    })
+
     app.use(cors({
         origin: true,
         credentials: true
@@ -27,8 +47,9 @@ AppDataSource.initialize().then(async () => {
 
     // start express server
     const PORT = env('SERVER_PORT', 3000);
-    app.listen(PORT)
+    // app.listen(PORT)
 
+    httpServer.listen(PORT)
 
     console.log(`Express server has started on port ${PORT}`)
 
