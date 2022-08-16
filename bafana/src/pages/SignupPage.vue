2a4b8ff93fe34a4cc91c12181be3d4eef40170af
Jibao Mansaray <jibaomansaray@gmail.com>
2022-08-16 06:19:28 +0000

    Add sign up page

---
+++ bafana/src/pages/SignupPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Sign up
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'SignupPage'
+})
+</script>
