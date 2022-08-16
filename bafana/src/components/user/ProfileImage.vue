f082dd1248d3cc7fe460a861cbec0bbffc28ccde
Jibao Mansaray <jibaomansaray@gmail.com>
2022-08-16 06:47:02 +0000

    Add user profile's image component

---
+++ bafana/src/components/user/ProfileImage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-img v-if="userStore.user" :src="'/static/user/' + userStore.user.username + '.png' ">
+  </q-img>
+</template>
+
+<script lang="ts">
+import { useUserStore } from 'src/stores/user-store'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'UserProfileImage',
+  setup () {
+    return {
+      userStore: useUserStore()
+    }
+  }
+})
+</script>
