c9d9e5bb35b3888ecade0818cf2263b63cd45e17
Jibao Mansaray <jibaomansaray@gmail.com>
2022-08-19 03:14:25 +0000

    Change how the drawer are by default
    The layout is now using the MainMenu component 😜

--- bafana/src/layouts/PlaneLayout.vue
+++ bafana/src/layouts/PlaneLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHr lpR lFr">
+  <q-layout view="lHr LpR lFr">
 
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
@@ -16,42 +16,22 @@
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="layoutStore.leftDrawer"  v-if="layoutStore.isLeftDrawerEnabled"  side="left" bordered>
+    <q-drawer show-if-above v-model="layoutStore.leftDrawer" side="left" bordered>
       <!-- drawer content -->
     </q-drawer>
 
-    <q-drawer show-if-above v-model="layoutStore.rightDrawer" v-if="layoutStore.isRightDrawerEnabled"  side="right" bordered>
-     <q-toolbar>
-       <q-toolbar-title>Main Menu</q-toolbar-title>
-     </q-toolbar>
-
-      <q-list padding class="rounded-borders text-primary">
-        <q-item clickable v-ripple v-for="item in menuStore.main" :key="item.label" :to="item.to">
-          <q-item-section avatar>
-            <q-icon :name="item.icon"></q-icon>
-          </q-item-section>
-          <q-item-section>{{item.label}}</q-item-section>
-        </q-item>
-        <q-separator />
-        <q-item-label header>Settings</q-item-label>
-        <q-separator />
-        <q-item clickable v-ripple v-for="item in menuStore.user" :key="item.label" :to="item.to">
-          <q-item-section avatar>
-            <q-icon :name="item.icon"></q-icon>
-          </q-item-section>
-          <q-item-section>{{item.label}}</q-item-section>
-        </q-item>
-      </q-list>
+    <q-drawer show-if-above v-model="layoutStore.rightDrawer" side="right" bordered>
+      <MainMenu></MainMenu>
     </q-drawer>
 
     <q-page-container>
-      <q-page class="q-pa-md">
-      <router-view />
-        <q-page-scroller expand position="top" :scroll-offset="150" :offset="[0, 0]">
+      <q-page padding>
+          <router-view />
+          <q-page-scroller expand position="top" :scroll-offset="150" :offset="[0, 0]">
             <div class="col cursor-pointer q-pa-sm bg-accent text-white text-center">
               Scroll back up...
             </div>
-        </q-page-scroller>
+          </q-page-scroller>
         </q-page>
     </q-page-container>
 
@@ -65,10 +45,11 @@ import { defineComponent } from 'vue'
 import { useRouter } from 'vue-router'
 import { useLayoutStore } from '../stores/layout-store'
 import ProfileImage from '../components/user/ProfileImage.vue'
+import MainMenu from '../components/general/MainMenu.vue'
 
 export default defineComponent({
   name: 'PlaneLayout',
-  components: { ProfileImage },
+  components: { ProfileImage, MainMenu },
   setup () {
     const router = useRouter()
     const userStore = useUserStore()
