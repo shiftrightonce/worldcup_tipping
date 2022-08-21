74723618478dcb85460cbe87e0f0ca35cbd56ec9
Jibao Mansaray <jibaomansaray@gmail.com>
2022-08-21 08:15:09 +0000

    Add logout page

---
+++ bafana/src/pages/LogoutPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="row">
+  <div class="col-md-4 col-xs-12" v-for="i in count" :key="i">
+    <h4>Hello world</h4>
+  </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'TestPage',
+  setup () {
+    return {
+      count: 300
+    }
+  }
+})
+</script>
