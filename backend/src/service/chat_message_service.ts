a26f8317352b38b2511f8646ca25d756d4473df3
Jibao Mansaray <jibaomansaray@gmail.com>
2022-08-21 09:25:07 +0000

    Add to specify the message type

--- backend/src/service/chat_message_service.ts
+++ backend/src/service/chat_message_service.ts
@@ -1,19 +1,20 @@
 import { In } from "typeorm";
 import { AppDataSource } from "../data-source";
-import { ChatMessage } from "../entity/ChatMessage";
+import { ChatMessage, ChatMessageType } from "../entity/ChatMessage";
 import { cleanUserData } from "./user_service";
 
 export const getChatMessageRepo = () => {
   return AppDataSource.getRepository(ChatMessage)
 }
 
-export const getRoomsLastMessage = async (roomIds: number[]): Promise<{ [roomId: number]: ChatMessage }> => {
+export const getRoomsLastMessage = async (roomIds: number[], messageType = ChatMessageType.MESSAGE): Promise<{ [roomId: number]: ChatMessage }> => {
   const result = {};
   (await getChatMessageRepo()
     .createQueryBuilder('message')
     .leftJoinAndSelect("message.room", "room")
     .leftJoinAndSelect("message.from", "user")
     .where({ room: In(roomIds) })
+    .where("message.type = :type", { type: messageType })
     .distinctOn(['message.roomId'])
     .orderBy({ 'message.createdAt': 'DESC' })
     .getMany()).forEach((message) => {
