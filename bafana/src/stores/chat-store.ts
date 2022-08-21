83772a6407049218cc85fb52434df2e4394e4e56
Jibao Mansaray <jibaomansaray@gmail.com>
2022-08-21 08:21:34 +0000

    Start work on the chat store

--- bafana/src/stores/chat-store.ts
+++ bafana/src/stores/chat-store.ts
@@ -1,3 +1,4 @@
+import { useAsyncState } from '@vueuse/core'
 import { defineStore } from 'pinia'
 import { useUserStore } from './user-store'
 
@@ -8,10 +9,28 @@ export enum ChatRoomType {
   PUBLIC = 'public'
 }
 
+export type ChatUser = {
+  id: number,
+  username: string,
+  avatar: string
+}
+
+export type ChatMessage = {
+  id: number,
+  createdAt: Date,
+  from: ChatUser,
+  message: string,
+  room: { id: number, name: string, type: ChatRoomType },
+  updatedAt: Date
+}
+
 export type ChatRoom = {
   id: number,
   name: string,
-  type: ChatRoomType
+  type: ChatRoomType,
+  lastMessage: null | ChatMessage,
+  members: ChatUser[],
+  avatar: string
 }
 
 export const useChatStore = defineStore('chatStore', {
@@ -19,11 +38,38 @@ export const useChatStore = defineStore('chatStore', {
     rooms: [] as ChatRoom[]
   }),
   actions: {
-    async fetchRooms () {
-      const response = await useUserStore().api.get(`${chatEndpoint}/my-rooms`)
+    fetchRooms () {
+      return useAsyncState(new Promise<ChatRoom[]>((resolve, reject) => {
+        useUserStore().api.get(`${chatEndpoint}/my-rooms`)
+          .then((response) => {
+            if (response.data.success) {
+              this.rooms = response.data.rooms as ChatRoom[]
+              this.rooms.forEach((room) => {
+                if (room.type === ChatRoomType.PUBLIC) {
+                  room.avatar = '/ph/group.png'
+                } else if (room.members.length > 0) {
+                  room.avatar = `/static/user/${room.members[0].username}.png`
+                  room.name = room.members[0].username
+                  room.members.forEach((member) => {
+                    member.avatar = `/static/user/${room.members[0].username}.png`
+                  })
+                } else {
+                  room.avatar = '/ph/noimage.png'
+                }
 
-      console.log('respose', response)
-      // todo
+                if (room.lastMessage) {
+                  room.lastMessage.createdAt = new Date(room.lastMessage.createdAt)
+                  room.lastMessage.updatedAt = new Date(room.lastMessage.updatedAt)
+                }
+              })
+            }
+            resolve(this.rooms)
+            console.log('respose', response.data)
+          }).catch((e) => {
+            // @todo handle error
+            reject(e)
+          })
+      }), [])
     }
   }
 })
