12b1968235c40f2fdf384ccde4697afd6aa9f092
Jibao Mansaray <jibaomansaray@gmail.com>
2022-08-22 04:01:40 +0000

    Only show admin menu for user with the role

--- bafana/src/components/general/MainMenu.vue
+++ bafana/src/components/general/MainMenu.vue
@@ -7,30 +7,39 @@
       <q-item-section avatar>
         <q-icon :name="item.icon"></q-icon>
       </q-item-section>
-      <q-item-section>{{item.label}}</q-item-section>
+      <q-item-section>{{ item.label }}</q-item-section>
     </q-item>
-    <q-separator />
-    <q-item-label header>Adminstration</q-item-label>
-    <q-separator />
+    <q-separator v-if="userStore.isAdmin" />
+    <q-item-label v-if="userStore.isAdmin" header>Adminstration</q-item-label>
+    <q-item clickable v-ripple v-for="item in menuStore.admin" :key="item.label" :to="item.to">
+      <q-item-section avatar>
+        <q-icon :name="item.icon"></q-icon>
+      </q-item-section>
+      <q-item-section>{{ item.label }}</q-item-section>
+    </q-item>
+    <q-separator v-if="userStore.isAdmin" />
     <q-item clickable v-ripple v-for="item in menuStore.user" :key="item.label" :to="item.to">
       <q-item-section avatar>
         <q-icon :name="item.icon"></q-icon>
       </q-item-section>
-      <q-item-section>{{item.label}}</q-item-section>
+      <q-item-section>{{ item.label }}</q-item-section>
     </q-item>
   </q-list>
 </template>
 
 <script lang="ts">
 import { useMenuStore } from 'src/stores/menu-store'
+import { useUserStore } from 'src/stores/user-store'
 import { defineComponent } from 'vue'
 export default defineComponent({
   name: 'MainMenu',
   setup () {
     const menuStore = useMenuStore()
+    const userStore = useUserStore()
 
     return {
-      menuStore
+      menuStore,
+      userStore
     }
   }
 })
