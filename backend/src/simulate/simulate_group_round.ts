50c270dee9439aa4f66f85c574fe30dff9e11d13
Jibao Mansaray <jibaomansaray@gmail.com>
2022-10-01 12:14:40 +0000

    We are now simulating group point system

--- backend/src/simulate/simulate_group_round.ts
+++ backend/src/simulate/simulate_group_round.ts
@@ -1,13 +1,60 @@
 import { AppDataSource } from "../data-source";
 import { MatchRound } from "../entity/Match";
-import { getMatchesByRound } from "../service/match_service";
+import { getCountryCount, updateCountry } from "../service/country_service";
+import { getGroupedMatches, getMatchesByRound } from "../service/match_service";
 import { run, getAllUsers, runTippingSimulation, runMatchPlaySimulation } from './simulate_general'
+import * as cliProgress from 'cli-progress';
+import { Country } from "../entity/Country";
 
 console.log(`
  |-----------------------------------------|\n
  |       simulating group round            |\n
  |-----------------------------------------|\n
 `);
+
+const runGroupPointsSimulation = async () => {
+  const groupedMatches = await getGroupedMatches();
+
+  const bar1 = new cliProgress.SingleBar({
+    clearOnComplete: false,
+  }, cliProgress.Presets.shades_classic);
+
+  console.log('Calculating countries\' points:\r');
+  bar1.start(await getCountryCount(), 0);
+  const countries: { [key: number]: Country } = {}
+  for (const group in groupedMatches) {
+    groupedMatches[group].forEach((match) => {
+      if (!countries[match.countryA.id]) {
+        countries[match.countryA.id] = match.countryA;
+        countries[match.countryA.id].groupPoints = (match.countryA.groupPoints < 0) ? 0 : match.countryA.groupPoints;
+      }
+      if (!countries[match.countryB.id]) {
+        countries[match.countryB.id] = match.countryB;
+        countries[match.countryB.id].groupPoints = (match.countryB.groupPoints < 0) ? 0 : match.countryB.groupPoints;
+      }
+
+
+      if (match.winner) {
+        if (match.winner.id === match.countryA.id) {
+          countries[match.countryA.id].groupPoints += 3;
+        } else {
+          countries[match.countryB.id].groupPoints += 3;
+        }
+      } else {
+        countries[match.countryA.id].groupPoints += 1;
+        countries[match.countryB.id].groupPoints += 1;
+      }
+    })
+
+  }
+
+  for (const country of Object.values(countries)) {
+    await updateCountry(country.id, { groupPoints: country.groupPoints });
+    bar1.increment(1)
+  }
+
+  bar1.stop();
+}
 
 
 AppDataSource.initialize().then(async () => {
@@ -19,7 +66,8 @@ AppDataSource.initialize().then(async () => {
   }
 
   if (run.match) {
-    await runMatchPlaySimulation(matches, MatchRound.GROUP)
+    await runMatchPlaySimulation(matches, MatchRound.GROUP);
+    await runGroupPointsSimulation();
   }
 
   AppDataSource.destroy();
