88616c23e79e83ac74f56656e4d45f275e13ede0
Jibao Mansaray <jibaomansaray@gmail.com>
2022-10-31 04:49:09 +0000

    Add the ability for user to subscribe/unsubscribe from push notification

--- backend/src/controller/UserController.ts
+++ backend/src/controller/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
+import { env } from "../data-source";
 import { getUserMatchTip } from "../service/tip_service"
-import { pluckUserFromRequest } from "../service/user_service";
+import {pluckUserFromRequest, getUserRepo } from "../service/user_service";
 
 export class UserController {
 
@@ -24,4 +25,34 @@ export class UserController {
       });
     }
   }
+
+  public async pushNotificationSubscribeAction (req: Request) {
+    const user = pluckUserFromRequest(req)
+    user.setData('push_subscription', req.body)
+
+    await getUserRepo().save(user)
+
+    return {
+      success: true
+    }
+  }
+
+  public async pushNotificationUnsubscribeAction (req: Request) {
+    const user = pluckUserFromRequest(req)
+    user.removeData('push_subscription')
+
+    await getUserRepo().save(user)
+
+    return {
+      success: true
+    }
+  }
+
+  public async vapidKeyAction () {
+    return {
+      success: true,
+      token: env('VAPID_PUBLIC_KEY')
+    }
+  }
+
 }
