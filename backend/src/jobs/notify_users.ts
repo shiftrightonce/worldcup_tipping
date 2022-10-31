9afd50eb30733ece65c0cd595041cfb30d87bed8
Jibao Mansaray <jibaomansaray@gmail.com>
2022-10-31 04:53:15 +0000

    Implemented notification queue worker

--- backend/src/jobs/notify_users.ts
+++ backend/src/jobs/notify_users.ts
@@ -1,21 +1,27 @@
 import { queueJob, createRegisterer } from './general'
+import { setupPushNotification, sendNotification } from "../push_notification";
+
+setupPushNotification()
 
 const handlerName = 'notify_users';
 
-export type Payload = {
-  title: string,
-  message: string,
-  code?: string
+export type JobPayload = {
+  message: {
+    title: string,
+    body: string,
+    icon?: string
+  },
+  user?: number
 }
 
-const processQueuedJob = async (job: Payload) => {
-  // @todo implement!!!
+const processQueuedJob = async (job: JobPayload) => {
+  await sendNotification(job.message, job.user)
   return true
 }
 
 
-export const addToQueue = (payload: Payload) => {
-  queueJob({ handler: handlerName, data: payload})
+export const addToQueue = (job: JobPayload) => {
+  queueJob({ handler: handlerName, data: job })
 }
 
 export default createRegisterer(handlerName, processQueuedJob)
