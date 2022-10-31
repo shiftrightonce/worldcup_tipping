e96d50474532bf9d52b181e4cda5e25ea033370c
Jibao Mansaray <jibaomansaray@gmail.com>
2022-10-31 05:03:02 +0000

    Prompt the user for notification permission

--- bafana/src/pages/ScoreBoardPage.vue
+++ bafana/src/pages/ScoreBoardPage.vue
@@ -26,28 +26,34 @@
       <ScrollUpMessage></ScrollUpMessage>
     </q-page-scroller>
   </q-page>
-  <q-page  v-else class="row items-evenly items-center flex-center">
-      <div class="col-12" style="text-align:center">
-        <span class="text-h6">Top position is still up for grabs!</span>
-      </div>
+  <q-page v-else class="row items-evenly items-center flex-center">
+    <div class="col-12" style="text-align:center">
+      <span class="text-h6">Top position is still up for grabs!</span>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
 import { useLayoutStore } from 'src/stores/layout-store'
 import { useTipStore } from 'src/stores/tip-store'
+import { useUserStore } from 'src/stores/user-store'
 import { defineComponent } from 'vue'
 import ScrollUpMessage from 'src/components/general/ScrollUpMessage.vue'
 
 export default defineComponent({
   setup () {
     const layoutStore = useLayoutStore()
+    const userStore = useUserStore()
     const { isLoading, isReady, state } = useTipStore().fetchScoreboard()
     let currentPosition = 1
     const positions: { [key: string]: number } = {}
 
     layoutStore.activeLeftDrawer(false)
     layoutStore.setTitle('Scoreboard')
+
+    setTimeout(() => {
+      userStore.setupNotificationSubscription()
+    }, 3000)
 
     const getPosition = (points: string | number): number => {
       if (positions[points]) {
