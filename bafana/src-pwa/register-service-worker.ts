f6ab93ab7df63539f133cf809937db1fd148d0f9
Jibao Mansaray <jibaomansaray@gmail.com>
2022-10-31 05:01:43 +0000

    Add a channel to communicate with the UI

--- bafana/src-pwa/register-service-worker.ts
+++ bafana/src-pwa/register-service-worker.ts
@@ -4,6 +4,23 @@ import { register } from 'register-service-worker'
 // events passes a ServiceWorkerRegistration instance in their arguments.
 // ServiceWorkerRegistration: https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration
 
+function urlBase64ToUint8Array (base64String: string) {
+  const padding = '='.repeat((4 - base64String.length % 4) % 4)
+  const base64 = (base64String + padding)
+    .replace(/-/g, '+')
+    .replace(/_/g, '/')
+
+  const rawData = atob(base64)
+  const outputArray = new Uint8Array(rawData.length)
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i)
+  }
+  return outputArray
+}
+
+const channel = new BroadcastChannel('world-cup-tipping')
+
 register(process.env.SERVICE_WORKER_FILE, {
   // The registrationOptions object will be passed as the second argument
   // to ServiceWorkerContainer.register()
@@ -11,8 +28,29 @@ register(process.env.SERVICE_WORKER_FILE, {
 
   // registrationOptions: { scope: './' },
 
-  ready (/* registration */) {
-    // console.log('Service worker is active.')
+  ready (registration) {
+    channel.onmessage = (event) => {
+      if (event.data) {
+        switch (event.data.type) {
+          case 'service:subscribe_to_notification':
+            {
+              const subscribeOptions = {
+                userVisibleOnly: true,
+                applicationServerKey: urlBase64ToUint8Array(event.data.data.vapid)
+              }
+              registration.pushManager.subscribe(subscribeOptions).then((response) => {
+                channel.postMessage({
+                  type: 'client:subscription_response',
+                  data: JSON.parse(JSON.stringify(response))
+                })
+              }).catch((e) => {
+                console.log('error subscribing to push notification', e)
+              })
+            }
+            break
+        }
+      }
+    }
   },
 
   registered (/* registration */) {
@@ -24,7 +62,12 @@ register(process.env.SERVICE_WORKER_FILE, {
   },
 
   updatefound (/* registration */) {
-    // console.log('New content is downloading.')
+    channel.postMessage({
+      type: 'client:app-update-found',
+      data: {
+        pending: true
+      }
+    })
   },
 
   updated (/* registration */) {
