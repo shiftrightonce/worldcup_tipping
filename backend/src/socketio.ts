5f9bbce8f58bcea2950f5055faf9dbd979787689
Jibao Mansaray <jibaomansaray@gmail.com>
2022-11-03 13:54:51 +0000

    Refactor the code and now listening for room data

--- backend/src/socketio.ts
+++ backend/src/socketio.ts
@@ -1,18 +1,43 @@
 import { Server } from "socket.io";
 import { authenticateToken } from './service/user_service'
 import { getRedisConnection } from './redis_client'
+import { getPublicRooms } from "./service/chat_room_service";
+import { ChatRoomType } from "./entity/ChatRoom";
+import { JobPayload } from "./jobs/process_chat_data";
+
+export const CHAT_MESSAGE_REDIS_QUEUE = 'chat:redis_queue';
+
+export const CHAT_ACTION_ROOM_MESSAGE = 'room_message';
 
 export default async (io: Server) => {
-    console.log("we are in here")
     const redisConnection = await getRedisConnection();
+    const rooms: Record<string, ChatRoomType> = {};
+    const publicRooms: string[] = [];
 
-    const val = await redisConnection.get('foo');
+    (await getPublicRooms()).forEach((room) => {
+        rooms[room.internalId] = room.type;
+        publicRooms.push(room.internalId);
+    })
 
-    console.log('redis foo value: ' + val);
-
-    if (!val) {
-        await redisConnection.set('foo', 'testing persisiting')
+    const onChatMessage = (data: JobPayload) => {
+        switch (data.action) {
+            // @todo handle specific action types
+            default:
+            io.to(data.roomId).emit('server:room_event', data);
+        }
     }
+
+    const listenForJob = () => {
+        redisConnection.executeIsolated(async (c) => {
+            const job = await c.blPop(CHAT_MESSAGE_REDIS_QUEUE, 0);
+            if (job.element) {
+                onChatMessage(JSON.parse(job.element));
+            }
+            setTimeout(listenForJob, 0)
+        })
+    }
+
+    setTimeout(listenForJob, 1000);
 
     io.use(async (socket, next) => {
         const error: Error & { data?: string } = new Error('unauthorized')
@@ -26,6 +51,18 @@ export default async (io: Server) => {
         if (!user) {
             return next(error)
         }
+
+        socket.on('disconnect', (reason) => {
+            console.log('disconnecting', reason)
+        });
+
+        // join public rooms
+        publicRooms.forEach((room) => {
+            socket.join(room);
+        })
+
+        // personal room
+        socket.join(user.internalId);
 
         return next()
     })
