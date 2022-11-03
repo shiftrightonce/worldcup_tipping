9869d3232bb71d35b4cb255ba2e7c6a35fc1537a
Jibao Mansaray <jibaomansaray@gmail.com>
2022-11-03 13:46:25 +0000

    Add `postMessage` function
    This function persist the room's message and push the
    message to the job queue.

--- backend/src/service/chat_message_service.ts
+++ backend/src/service/chat_message_service.ts
@@ -1,7 +1,11 @@
 import { In } from "typeorm";
 import { AppDataSource } from "../data-source";
 import { ChatMessage, ChatMessageType } from "../entity/ChatMessage";
-import { cleanUserData } from "./user_service";
+import { ChatRoomType } from "../entity/ChatRoom";
+import { User } from "../entity/User";
+import { queueRoomMessage } from "../jobs/process_chat_data";
+import { findChatRoomById, findChatRoomByInternalId } from "./chat_room_service";
+import { cleanUserData, getUserById } from "./user_service";
 
 export const getChatMessageRepo = () => {
   return AppDataSource.getRepository(ChatMessage)
@@ -19,9 +23,32 @@ export const getRoomsLastMessage = async (roomIds: number[], messageType = ChatM
     .orderBy({ 'message.createdAt': 'DESC' })
     .getMany()).forEach((message) => {
       message.from = cleanUserData(message.from)
-      result[message.room.id] = message
+      result[message.room.internalId] = message
     })
 
   return result
 
+}
+
+export const postMessage = async (roomId: string | number, sender: User | number, message: string, type = ChatMessageType.MESSAGE) => {
+  let chatMessage = new ChatMessage()
+
+  const room = (typeof roomId === 'string') ? await findChatRoomByInternalId(roomId) : await findChatRoomById(roomId)
+  const user = (typeof sender === 'number') ? await getUserById(sender) : sender;
+
+  if (user && room) {
+
+    chatMessage.type = type
+    chatMessage.room = room;
+    chatMessage.from = user
+    chatMessage.message = message
+
+    chatMessage = await getChatMessageRepo().save(chatMessage);
+    chatMessage.from = cleanUserData(chatMessage.from);
+    setTimeout(() => {
+      queueRoomMessage(room.internalId, chatMessage);
+    }, 1000)
+  }
+
+  return chatMessage;
 }
