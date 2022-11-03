5b0f7ac081d2d6cd194a5d0ef31195970dd4f1a9
Jibao Mansaray <jibaomansaray@gmail.com>
2022-11-03 13:28:12 +0000

    Create chat bubble component

---
+++ bafana/src/components/chat/ChatBubble.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-chat-message
+    :name="(data.from.id === userStore.user?.id)? 'me' : data.from.username"
+    :avatar="data.from.avatar"
+    :text="[data.message]"
+    :stamp="data.createdAt"
+    :sent="data.from.id === userStore.user?.id"
+   />
+</template>
+
+<script lang="ts">
+import { ChatMessage } from 'src/stores/chat-store'
+import { useUserStore } from 'src/stores/user-store'
+import { defineComponent, PropType } from 'vue'
+
+export default defineComponent({
+  name: 'ChatBubble',
+  props: {
+    data: {
+      type: Object as PropType<ChatMessage>,
+      required: true
+    },
+    from: {
+      type: Boolean as PropType<boolean>,
+      default: false
+    }
+  },
+  setup () {
+    const userStore = useUserStore()
+
+    return {
+      userStore
+    }
+  }
+})
+</script>
