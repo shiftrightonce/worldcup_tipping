89578e30773cf29c1216a52344afb5e9c2035fc4
Jibao Mansaray <jibaomansaray@gmail.com>
2022-11-03 14:00:44 +0000

    Add the chat bubble component

--- bafana/src/pages/GroupChatPage.vue
+++ bafana/src/pages/GroupChatPage.vue
@@ -1,22 +1,30 @@
 <template>
-  <transition
-    appear
-    enter-active-class="animated slideInLeft"
-    leave-active-class="animated slideOutRight"
-  >
+  <transition appear enter-active-class="animated slideInLeft" leave-active-class="animated slideOutRight">
     <div class="row">
       <div class="col-md-4 col-xs-12">
         <div>Group Chat</div>
+        <div v-if="chatStore.currentRoom && chatStore.messages[chatStore.currentRoom.internalId]">
+          <ChatBubble v-for="message in chatStore.messages[chatStore.currentRoom.internalId]" :key="message.internalId"
+            :data="message"></ChatBubble>
+        </div>
       </div>
     </div>
   </transition>
 </template>
 
 <script lang="ts">
+import ChatBubble from 'src/components/chat/ChatBubble.vue'
+import { useChatStore } from 'src/stores/chat-store'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'GroupChatPage'
+  name: 'GroupChatPage',
+  setup () {
+    const chatStore = useChatStore()
+    return {
+      chatStore
+    }
+  },
+  components: { ChatBubble }
 })
-
 </script>
