9fc0145a2868ce6a514197e881ea2040730bc081
Jibao Mansaray <jibaomansaray@gmail.com>
2022-11-03 13:32:30 +0000

    Remove the unused `Response` instance

--- backend/src/controller/ChatController.ts
+++ backend/src/controller/ChatController.ts
@@ -1,9 +1,11 @@
-import { Request, Response } from "express";
+import { Request } from "express";
+import { ChatMessageType } from "../entity/ChatMessage";
+import { postMessage } from "../service/chat_message_service";
 import { getUserRooms } from "../service/chat_room_service";
 import { pluckUserFromRequest } from "../service/user_service";
 
 export class ChatController {
-  public async getMyRoomsAction (req: Request, res: Response) {
+  public async getMyRoomsAction (req: Request) {
     const user = pluckUserFromRequest(req)
 
     return {
