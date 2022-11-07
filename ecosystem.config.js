5a2b3276fd63bea7164466dcb428de4c0f5eacfa
Jibao Mansaray <jibaomansaray@gmail.com>
2022-11-07 12:53:33 +0000

    Add `PM2` ecosystem configuration file

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: 'backend',
+      cwd: './backend',
+      script: 'npm',
+      args: 'run start'
+    },
+    {
+      name: 'queue',
+      cwd: './backend',
+      script: 'npm',
+      args: 'run queue:start'
+    }
+  ]
+};
