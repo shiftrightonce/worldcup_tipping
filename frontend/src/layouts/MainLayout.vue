5bdf88d39293f2e70f93c7ad70e74420b2d5b88d
Jibao Mansaray <jibaomansaray@gmail.com>
2022-11-10 04:43:15 +0000

    Move some of the pages instead

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,16 +1,41 @@
 <template>
-  <q-layout>
-    <q-header>
-      <q-toolbar>
-        <q-toolbar-title>
-          {{ layoutStore.title }}
-        </q-toolbar-title>
+  <q-layout view="hHr lpr fFr">
+    <q-drawer show-if-above v-model="layoutStore.rightDrawer" side="right" overlay behavior="mobile" bordered>
+      <MainMenu></MainMenu>
+    </q-drawer>
 
-        <div>v0.0.1</div>
-      </q-toolbar>
-    </q-header>
     <q-page-container>
-      <router-view />
+      <q-page class="row">
+        <div class="col-12 q-pb-md" style="background-color: #056480;max-height: 110px;">
+          <div class="row justify-evenly">
+            <div class="col-xs-12 col-md-8 items-center">
+              <q-toolbar class="text-white q-mt-md">
+                <AppLogo size="70px"></AppLogo>
+                <q-toolbar-title class="text-positive text-bold">
+                  MansTip
+                </q-toolbar-title>
+                <div class="gt-sm">
+                  <div v-if="!userStore.isLogin">
+                  <q-btn flat v-for="item in menuStore.public" :to="item.to" :icon="item.icon" :key="item.label">
+                    &nbsp;{{ item.label }}</q-btn>
+                    <q-btn v-if="menuStore.shareIsSupported()" flat @click="shareApp" label="Share" icon="share"></q-btn>
+                  </div>
+                  <div v-else>
+                <q-btn flat round dense icon="menu" class="gt-sm"
+                  @click="() => layoutStore.rightDrawer = !layoutStore.rightDrawer" />
+                  </div>
+                </div>
+                <q-btn flat round dense icon="menu" class="lt-md"
+                  @click="() => layoutStore.rightDrawer = !layoutStore.rightDrawer" />
+              </q-toolbar>
+            </div>
+          </div>
+        </div>
+        <router-view />
+        <div class="col-12" style="background-color: #056480;">
+          <FooterComponent></FooterComponent>
+        </div>
+      </q-page>
     </q-page-container>
   </q-layout>
 </template>
@@ -18,19 +43,36 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useLayoutStore } from '../stores/layout-store'
+import AppLogo from 'src/components/general/AppLogo.vue'
+import MainMenu from 'src/components/general/MainMenu.vue'
+import { useMenuStore } from 'src/stores/menu-store'
+import FooterComponent from 'src/components/general/FooterComponent.vue'
+import { useUserStore } from 'src/stores/user-store'
 
 export default defineComponent({
   name: 'MainLayout',
-
   components: {
+    AppLogo,
+    MainMenu,
+    FooterComponent
   },
   setup () {
     const layoutStore = useLayoutStore()
+    const menuStore = useMenuStore()
+    const userStore = useUserStore()
 
     layoutStore.setTitle('Home')
 
+    const shareApp = () => {
+      menuStore.shareApp()
+    }
+
     return {
-      layoutStore
+      layoutStore,
+      menuStore,
+      userStore,
+
+      shareApp
     }
   }
 })
