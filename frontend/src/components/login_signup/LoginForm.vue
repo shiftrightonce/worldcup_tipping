5472356c21f56f1e94aa214f77fb5d08fd77001d
Jibao Mansaray <jibaomansaray@gmail.com>
2022-11-10 04:41:00 +0000

    Add header and some spacing

--- frontend/src/components/login_signup/LoginForm.vue
+++ frontend/src/components/login_signup/LoginForm.vue
@@ -1,6 +1,9 @@
 <template>
   <q-form @submit="onSignIn" autofocus>
     <div class="row">
+      <div class="col-xs-12 col-md-12 q-mt-md q-pl-lg q-pr-lg q-mb-md">
+        <div class="text-h4">Login in</div>
+      </div>
       <div class="col-xs-12 col-md-12 q-pl-lg q-pr-lg q-mb-md">
         <q-input v-model="username" label="Username" outlined @update:model-value="onInput"></q-input>
       </div>
@@ -8,18 +11,15 @@
         <q-input v-model="password" label="Password" outlined @update:model-value="onInput"
           :type="isPassword ? 'password' : 'text'">
           <template v-slot:append>
-            <q-icon :name="isPassword? 'visibility_off' : 'visibility'" class="cursor-pointer"
-              @click="isPassword= !isPassword" />
+            <q-icon :name="isPassword ? 'visibility_off' : 'visibility'" class="cursor-pointer"
+              @click="isPassword = !isPassword" />
           </template>
         </q-input>
       </div>
-      <div class="col-xs-6 q-pl-lg q-pr-lg q-mb-md">
-        <q-btn label="Sign up" color="secondary" align="left" :to="{ name: 'signup'}"></q-btn>
+      <div class="col-xs-12 q-pl-lg q-pr-lg q-mb-md">
+        <q-btn label="Login" color="primary" class="float-right" type="submit"></q-btn>
       </div>
-      <div class="col-xs-6 q-pl-lg q-pr-lg q-mb-md">
-        <q-btn label="Sign in" color="primary" class="float-right" type="submit"></q-btn>
-      </div>
-      <div class="col-xs-12 q-pl-lg q-pr-lg q-mt-lg">
+      <div class="col-xs-12 q-pl-lg q-pr-lg q-mt-lg q-mb-md">
         <div class="text-caption">
           <q-btn flat dense label="Having issue logging in? Click here" no-caps :to="{ name: 'forgot-login' }"></q-btn>
         </div>
@@ -72,6 +72,7 @@ export default defineComponent({
             void await userStore.login(username.value, password.value)
             router.push({ name: 'scoreboard' })
           } catch (e) {
+            // @todo Make error message more helpful
             $q.dialog({
               title: 'Authentication failed',
               message: 'Username or password incorrect'
