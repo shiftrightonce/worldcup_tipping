4ad367af61c3634a09b434ca44bc209c36a1ed16
Jibao Mansaray <jibaomansaray@gmail.com>
2022-11-15 11:11:55 +0000

    Add job that sends password reset email

---
+++ packages/backend/src/jobs/password_request_email.ts
@@ -0,0 +1,64 @@
+import { env } from '../data-source';
+import { updateUser } from '../service/user_service';
+import { queueJob, createRegisterer } from './general'
+
+const sendgrid = require('@sendgrid/mail')
+const handlerName = 'password_reset_email';
+const fromEmail = env('SENDER_EMAIL')
+const sendgridToken =env('SENDGRID_API_KEY') 
+
+sendgrid.setApiKey(sendgridToken)
+
+export type JobPayload = {
+  userId: number
+}
+
+function generateRandomString (length: number) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+
+const processQueuedJob = async (job: JobPayload) => {
+
+  try {
+    const result = await updateUser(job.userId, {
+      password: generateRandomString(8)
+    });
+    if (result.success && fromEmail && sendgridToken) {
+      const url = `${env('APP_URL')}#/forgot-login?_t=${result.user.token}`
+      await sendgrid.send({
+        to: 'jibaomansaray@gmail.com',
+        from: 'support@911tutor.com',
+        subject: 'MansarTip Password reset',
+        html: `
+        Hello ${result.user.username},
+        <p>
+          You requested a password reset. Please click the link below in order to complete the process.
+        </p>
+        <p>
+          <a href="${url}" _target="blank">Click Here</a> or copy and paste the this link <br/> <br/>
+          ${url}
+        </p>
+        <p>
+          Regards,
+          J-man
+        </p>
+        `
+      })
+    }
+  } catch (e) {
+    console.log(e.message)
+  }
+
+  return true;
+}
+
+export const addToQueue = (userId: number) => {
+  queueJob({ handler: handlerName, data: { userId } });
+}
+
+export default createRegisterer(handlerName, processQueuedJob);
