c3e583d917541b82098b7574286a7ce6c9ea4d0a
Jibao Mansaray <jibaomansaray@gmail.com>
2022-11-21 05:05:00 +0000

    Place an outter loop around the queue runner

--- packages/backend/src/jobs/queue.ts
+++ packages/backend/src/jobs/queue.ts
@@ -6,7 +6,7 @@ import notifyUsers from './notify_users'
 import updateRound16MatchCountries from './update_round_16_match_countries'
 import updateRound8MatchCountries from './update_round_8_match_countries'
 import updateRound4MatchCountries from './update_round_4_match_countries'
-import updateThirdAndFinalMatchCountries from  './update_third_and_final_place_matches_countries'
+import updateThirdAndFinalMatchCountries from './update_third_and_final_place_matches_countries'
 import processChatData from './process_chat_data'
 import processPasswordResetReqest from './password_request_email'
 import { AppDataSource } from '../data-source'
@@ -26,31 +26,35 @@ processChatData(processors);
 processPasswordResetReqest(processors);
 
 
-AppDataSource.initialize().then(async () => {
-  getRedisConnection(true)
-    .then(async (client) => {
-      client.executeIsolated(async (c) => {
-        do {
-          const job = await c.blPop(
-            QUEUE_NAME,
-            0
-          );
-          const theJob = job.element ? JSON.parse(job.element) as Job : { handler: 'unknown', data: {} }
+setTimeout(() => {
+  while (true) {
+    AppDataSource.initialize().then(async () => {
+      getRedisConnection(true)
+        .then(async (client) => {
+          client.executeIsolated(async (c) => {
+            do {
+              const job = await c.blPop(
+                QUEUE_NAME,
+                0
+              );
+              const theJob = job.element ? JSON.parse(job.element) as Job : { handler: 'unknown', data: {} }
 
-          if (processors[theJob.handler]) {
-            try {
-              await processors[theJob.handler](theJob.data)
-            } catch (e) {
-              console.log('error processing job', e.message)
-            }
-          }
-        } while (true)
-      })
-    })
-    .catch((e) => {
-      console.log('error connecting to redis: ' + e.message)
-    })
+              if (processors[theJob.handler]) {
+                try {
+                  await processors[theJob.handler](theJob.data)
+                } catch (e) {
+                  console.log('error processing job', e.message)
+                }
+              }
+            } while (true)
+          })
+        })
+        .catch((e) => {
+          console.log('error connecting to redis: ' + e.message)
+        })
 
-}).catch(error => console.log(error))
+    }).catch(error => console.log(error))
+  }
+}, 0);
 
 console.log("Queue is up and running \r\n")
